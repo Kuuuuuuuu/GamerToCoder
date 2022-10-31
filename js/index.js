ec96938e4b3941123d52142a0d0772b11bc8501f
Nayuki <notkungz.suphakorn@gmail.com>
2022-10-31 22:21:17 +0700

    omg wtf

--- js/index.js
+++ js/index.js
@@ -112,25 +112,4 @@ function loadCharacters() {
     });
 }
 
-document.addEventListener('click', applyCursorRippleEffect);
-
-function applyCursorRippleEffect(t) {
-    const e = document.createElement("div");
-    e.className = "ripple";
-    document.body.appendChild(e);
-    e.style.left = t.clientX + "px";
-    e.style.top = t.clientY + "px";
-    e.style.animation = "ripple-effect .4s  linear";
-    e.onanimationend = () => {
-        document.body.removeChild(e);
-    }
-}
-
-try {
-    const c = document.body.getElementsByTagName("*");
-    for (let t of c) t.onclick = t => t.stopPropagation();
-} catch (t) {
-    console.log(t);
-}
-
 // actually this code is very shit and super unoptimized
