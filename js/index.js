c43259697788d7c361219fb554023ea5a4cb5d67
Nayuki <notkungz.suphakorn@gmail.com>
2022-10-31 22:30:57 +0700

    omg wtf

--- js/index.js
+++ js/index.js
@@ -42,6 +42,7 @@ function start() {
                 document.getElementById("title").style.display = "block";
                 document.getElementById("characterslist").style.display = "inline-block";
                 document.getElementById("bg").style.display = "block";
+                document.getElementById("hack").style.display = "none";
                 document.getElementById("footer").style.display = "block";
                 clearInterval(interval);
             }
@@ -111,5 +112,6 @@ function loadCharacters() {
         }
     });
 }
+
 
 // actually this code is very shit and super unoptimized
